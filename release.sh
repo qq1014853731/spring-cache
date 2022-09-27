356a2c8af8b65447ef37d42a7fddf1c3d87f6902
chukongxiang <1014853731@qq.com>
2022-09-28 00:24:21 +0800

    更新发布脚本注释

--- release.sh
+++ release.sh
@@ -3,4 +3,5 @@
 
 # 3. 将版本修改为 RELEASE 或 空 （RELEASE版本只允许发布一次）
 # 4. mvn clean deploy -P release -Dmaven.test.skip=true
+# 如果下面这条命令发布失败，请检查是否已经发布到了仓库，注意：是否开启了自动发布（autoReleaseAfterClose）
 # 5. mvn -P release org.sonatype.plugins:nexus-staging-maven-plugin:1.6.8:release
