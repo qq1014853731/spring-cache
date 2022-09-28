7b87a0a3592f1817538e22e88253ce218bbd7888
chukongxiang <1014853731@qq.com>
2022-09-28 14:08:01 +0800

    调整自动装配

--- src/main/java/top/chukongxiang/spring/cache/core/SpringCacheAutoConfiguration.java
+++ src/main/java/top/chukongxiang/spring/cache/core/SpringCacheAutoConfiguration.java
@@ -3,6 +3,7 @@ package top.chukongxiang.spring.cache.core;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
@@ -29,27 +30,30 @@ public class SpringCacheAutoConfiguration implements ImportBeanDefinitionRegistr
 
         // 手动注册BEAN
         registerSyntheticBeanIfMissing(registry, SpringCacheManager.class,
-                ExpiresCacheManager.class);
+                ExpiresCacheManager.class, false);
         registerSyntheticBeanIfMissing(registry, SpringCacheTemplate.class,
-                SpringCacheTemplate.class);
+                SpringCacheTemplate.class, false);
         registerSyntheticBeanIfMissing(registry, SpringCacheAop.class,
-                SpringCacheAop.class);
+                SpringCacheAop.class, true);
     }
 
     /**
      * 检查并注册Bean
-     * @param registry
-     * @param name
-     * @param beanClass
+     * @param registry 注册器
+     * @param beanClass 要注册的Bean，
+     * @param defaultBeanClass 该Bean的实现类
+     * @param synthetic 该Bean是否是用户自定义的bean，（bean的加载顺序）
      */
     private void registerSyntheticBeanIfMissing(BeanDefinitionRegistry registry,
-                                                Class<?> beanClass, Class<?> defaultBeanClass) {
+                                                Class<?> beanClass,
+                                                Class<?> defaultBeanClass, boolean synthetic) {
         // 检查指定类型的Bean name数组是否存在，如果不存在则创建Bean并注入到容器中
         if (ObjectUtils.isEmpty(
                 ((ConfigurableListableBeanFactory)this.beanFactory).getBeanNamesForType(beanClass, true, false))) {
             RootBeanDefinition beanDefinition = new RootBeanDefinition(defaultBeanClass);
-            beanDefinition.setSynthetic(true);
-            registry.registerBeanDefinition(beanClass.getName(), beanDefinition);
+            beanDefinition.setSynthetic(synthetic);
+            String beanName = BeanDefinitionReaderUtils.generateBeanName(beanDefinition, registry);
+            registry.registerBeanDefinition(beanName, beanDefinition);
         }
     }
 
